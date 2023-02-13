73005746eed8281c8b83e01cb2129b7cd6392f9b
Лилиана <lilya_gazina@mail.ru>
2023-02-13 10:27:57 +0300

    готова

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] nums = {1, 2, 3, 5, 6, 7, 8};
+        for (int i = nums.length - 1; i >= 0; i--) {
+            System.out.print(nums[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+
+        }
     }
 }
